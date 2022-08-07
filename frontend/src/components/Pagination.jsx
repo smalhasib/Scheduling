14cd59c00b4323f3fae67c6a3badd1df8cde4b07
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2022-08-07 20:17:30 +0600

    pagination complete

--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -1,24 +1,29 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useState } from "react";
 
 const Pagination = ({ paginate, totalProf, profPerPage }) => {
+  const [pageColor, setPageColor] = useState(1);
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalProf / profPerPage); i++) {
     pageNumbers.push(i);
   }
+
   return (
     <>
-      <nav className="w-full flex justify-center">
-        <ul className="inline-flex -space-x-px mt-5">
+      <nav className="w-full flex justify-center mt-8 rounded-md">
+        <ul className="inline-flex -space-x-px ">
           {pageNumbers.map((number) => (
             <li key={number}>
               <a
                 onClick={() => {
                   paginate(number);
+                  setPageColor(number);
                 }}
                 href="#"
-                className="py-4  px-5 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                className={`py-4  px-5 leading-tight text-gray-500 ${
+                  pageColor === number ? "bg-gray-600 text-white" : "bg-white"
+                }  border border-gray-300 hover:bg-gray-400 hover:text-white dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
               >
                 {number}
               </a>
